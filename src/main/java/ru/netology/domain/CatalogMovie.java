39de896d78622fd5facdc186e01ab207379f6b7f
Elena Gorshenina <elenkagorshenina19.07.14@gmail.com>
2021-08-01 12:13:12 +0300

    Adding Movie

---
+++ src/main/java/ru/netology/domain/CatalogMovie.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.netology.manager.MovieManager;
+
+@AllArgsConstructor
+public class CatalogMovie {
+  private int id;
+  private int movieId;
+  private String movieUrlPicture;
+  private String movieName;
+  private String movieGenre;
+  private String dataPremiere;
+
+}
